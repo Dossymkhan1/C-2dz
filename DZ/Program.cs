25fafface11401aab1c29a2d7f8923ae76e501a1
Your_name <dosss_xa@example.com>
2022-12-04 16:24:50 +0600

    added new dz1

---
+++ DZ/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+Console.WriteLine("Введите 2 числа (после каждого нажмите Enter)");
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+if (number1 > number2)
+{
+    Console.WriteLine("Наибольшее число - " + number1 + ",наименьшее число - " + number2);
+}
+else
+{
+    Console.WriteLine("Наибольшее число - " + number2 + ",наименьшее число - " + number1);
+}
